d0bb26b6840c8bb92da39783b3ab72dfc812df6f
macaul_b <budmacaulay97@outlook.com>
2025-12-09 13:46:04 +0100

    update optimade img

--- src/components/OptimadeClient/OptimadeHeader.jsx
+++ src/components/OptimadeClient/OptimadeHeader.jsx
@@ -2,7 +2,7 @@ export default function OptimadeHeader() {
   return (
     <div className="flex justify-center">
       <img
-        src="optimade_logo.png"
+        src="/optimade_logo-min.png"
         alt="OptimadeLogo"
         className="h-18 sm:h-24 md:h-32 w-auto object-contain"
       />
